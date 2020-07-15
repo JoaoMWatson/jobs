d4fb17d30a3bf5fd0a0190da3b29d42c0c100e82
JoaoMWatson <joaopedrowatson@hotmail.com>
2020-07-15 06:09:20 -0300

    Teste terminado

--- scicrop-api-test-python.py
+++ scicrop-api-test-python.py
@@ -1,2 +1,28 @@
+from datetime import datetime as dt
+from json import load
+import requests
+
+
+def getResume():
+	with open('resume.json') as f:
+		resume = load(f)
+	return resume
+
+def main():
+	URL = 'https://engine.scicrop.com/scicrop-engine-web/api/v1/jobs/post_resume'
+
+	resume = getResume()
+
+	try:
+		response = requests.post(url=URL, data=resume)
+		if response.status_code == 200:
+			print("Okay")
+	except:
+		print("error")
+		print(response.status_code)
+	finally:
+		print(resume)
+		
+
 if __name__ == '__main__':
-	pass
+	main()
